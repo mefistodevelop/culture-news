2ce883bcabd9d4990c040d0d30dabef8df961236
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-04-17 21:34:04 +0300

    feat: add blocks to the page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './scss/main.scss';
+
+function requireAll(requireContext) {
+  return requireContext.keys().map(requireContext);
+}
+
+requireAll(require.context('./components', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));
