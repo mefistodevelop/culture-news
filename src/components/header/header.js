4744f2ac1a5049dc7edbfffb14bf2ad0ffcf124c
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-04-17 21:34:49 +0300

    feat: make header responsive

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,1 +1,27 @@
 import './header.scss';
+
+import $ from 'jquery';
+
+$(document).ready(() => {
+
+  const $navigation = $('.js-header__navigation');
+  const $burger = $('.js-header__burger');
+  const $closeButton = $('.js-header__close-button');
+  const $fade = $('.fade');
+  const $navigationVisibilityClass = 'header__navigation_visible';
+  const $fadeVisibilityClass = 'fade_visible';
+ 
+
+  const openMenu = () => {
+    $navigation.addClass($navigationVisibilityClass);
+    $fade.addClass($fadeVisibilityClass);
+  }
+
+  const closeMenu = () => {
+    $navigation.removeClass($navigationVisibilityClass);
+    $fade.removeClass($fadeVisibilityClass);
+  }
+
+  $burger.on('click', openMenu);
+  $closeButton.on('click', closeMenu);
+});
